07d8bf6e415bc431eaf57502639cc4aa047c92ec
Alyson Amourette <alyson.amourette@gmail.com>
2025-02-23 21:18:11 +0100

    [sale/price] Allow 0 as investment price (prev. was > 0 only)

--- src/Dto/SaleCreateDto.php
+++ src/Dto/SaleCreateDto.php
@@ -9,14 +9,14 @@ final class SaleCreateDto {
     #[Assert\Range(min: 1)]
     public int $quantity;
 
-    #[Assert\Range(min: 1)]
-    public int $price;
+    #[Assert\Range(min: 0)]
+    public int $price = 0;
 
     #[Assert\Range(min: 1)]
     public int $sellPrice = 1;
 
-    #[Assert\Range(min: 1)]
-    public float $taxPrice = 1;
+    #[Assert\Range(min: 0)]
+    public float $taxPrice = 0;
 
     public ?bool $sold = false;
 
--- src/Dto/SaleUpdateDto.php
+++ src/Dto/SaleUpdateDto.php
@@ -8,14 +8,14 @@ final class SaleUpdateDto {
     #[Assert\Range(min: 1)]
     public int $quantity;
 
-    #[Assert\Range(min: 1)]
-    public int $price;
+    #[Assert\Range(min: 0)]
+    public int $price = 0;
 
     #[Assert\Range(min: 1)]
     public int $sellPrice = 1;
 
-    #[Assert\Range(min: 1)]
-    public int $taxPrice = 1;
+    #[Assert\Range(min: 0)]
+    public float $taxPrice = 0;
 
     public ?bool $sold;
 }
